b47c78dc56cfb6e482255caf1412086b97e759da
ss-aa123 <neermas1@hotmail.com>
2022-11-29 18:57:04 -0500

    established connection with client.js

--- play.js
+++ play.js
@@ -1,19 +1,5 @@
-const net = require("net");
 
-// establishes a connection with the game server and returns an object
-const connect = function () {
-  const conn = net.createConnection({
-    host:  localhost,
-    port: 50541,
-  });
-
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log('Got your data')
-  });  return conn;
-};
+const {connect} = require('./client')
 
 console.log("Connecting ...");
 connect();
