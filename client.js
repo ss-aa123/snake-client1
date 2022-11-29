f08a405779a760d36ae7152471d8276fa94c2cba
ss-aa123 <neermas1@hotmail.com>
2022-11-29 18:56:35 -0500

    added in a connection to play.js and successfully snake and message showed up on screen

---
+++ client.js
@@ -0,0 +1,29 @@
+
+const net = require("net");
+
+// establishes a connection with the game server and returns an object
+const connect = function () {
+  const conn = net.createConnection({
+    host:  'localhost',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log('Successfully connected to a game server!')
+    conn.write('Name: SAM')
+  });  
+
+  conn.on("data", (data) => {
+    console.log("Name: SAM", data);
+  });
+
+  return conn;
+};
+
+
+module.exports = {
+  connect
+};
