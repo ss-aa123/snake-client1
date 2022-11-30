8780c5d604d2e3693c65c43fc46c9677d0ea823f
ss-aa123 <neermas1@hotmail.com>
2022-11-29 23:15:43 -0500

    adding WASD functionality movements and implementing special keyed unique messages for everyone to see while playing

--- input.js
+++ input.js
@@ -1,5 +1,13 @@
+const { connect } = require("http2");
+const { createConnection } = require("net");
+
+// Stores the active TCP connection object.
+let connection; 
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,10 +21,31 @@ const setupInput = function () {
   return stdin;
 };
 
+
 const handleUserInput = function (key) {
   const stdout = process.stdout;
   if (key === '\u0003') {
     process.exit();
+  }
+  //HOW TO GET THESE TO WORKKKKKKK
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+
+  if (key === 'q') {
+    connection.write("Say: Thanks for playing!")
+  }
+  if (key === 'z') {
+    connection.write("Say: Good Job!")
   }
 };
 
